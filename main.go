77c671dfda3dda7f152fa248dd87f4ac869e8681
Ben Sidhom <bsidhom@gmail.com>
2022-11-05 00:22:42 -0700

    Add option to serve TLS

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"flag"
 	"fmt"
 	"log"
+	"math/big"
+	"net"
 	"net/http"
 	"time"
 )
@@ -13,10 +21,12 @@ func main() {
 	var port int
 	var allInterfaces bool
 	var enableCache bool
+	var tlsName string
 	flag.StringVar(&dir, "from", ".", "Directory to serve data from.")
 	flag.IntVar(&port, "port", 8000, "Port to listen on.")
 	flag.BoolVar(&allInterfaces, "public", false, "Listen on all (including non-localhost) interfaces.")
 	flag.BoolVar(&enableCache, "cache", false, "Enable caching")
+	flag.StringVar(&tlsName, "tls", "", "If non-empty, will generate a random TLS keypair for the given server name and serve TLS.")
 	flag.Parse()
 
 	handler := http.FileServer(http.Dir(dir))
@@ -33,8 +43,22 @@ func main() {
 		addr = fmt.Sprintf("localhost:%d", port)
 	}
 
+	var listener net.Listener
+	var err error
+	if tlsName == "" {
+		listener, err = net.Listen("tcp", addr)
+	} else {
+		cert := generateTLSCert(tlsName)
+		listener, err = tls.Listen("tcp", addr, &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		})
+	}
+	if err != nil {
+		panic(err)
+	}
+
 	log.Printf("listening on %q", addr)
-	err := http.ListenAndServe(addr, mux)
+	err = http.Serve(listener, mux)
 	if err != nil {
 		panic(err)
 	}
@@ -62,4 +86,47 @@ func disableCache(h http.Handler) http.Handler {
 		w.Header().Del("Last-Modified")
 		w.Header().Del("Date")
 	})
+}
+
+func generateTLSCert(name string) tls.Certificate {
+	// Generate a one-time use TLS certificate. Adapted from https://stackoverflow.com/a/43828190.
+	key, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		panic(err)
+	}
+	// Generate a pem block with the private key
+	keyPem := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	})
+	maxSerialNumber := big.NewInt(1)
+	maxSerialNumber.Lsh(maxSerialNumber, 256)
+	serialNumber, err := rand.Int(rand.Reader, maxSerialNumber)
+	if err != nil {
+		panic(err)
+	}
+	tml := x509.Certificate{
+		NotBefore: time.Now(),
+		// NOTE: This cert will expire in 1 day.
+		NotAfter:     time.Now().Add(24 * time.Hour),
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			CommonName:   name,
+			Organization: []string{"Local Development"},
+		},
+		BasicConstraintsValid: true,
+	}
+	cert, err := x509.CreateCertificate(rand.Reader, &tml, &tml, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	certPem := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert,
+	})
+	tlsCert, err := tls.X509KeyPair(certPem, keyPem)
+	if err != nil {
+		panic(err)
+	}
+	return tlsCert
 }
